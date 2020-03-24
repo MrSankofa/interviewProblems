23a72853330e7e8750c9815dec8d00ec36b51263
Brett Cunningham <brett.cunningham@sbevision.com>
2020-03-24 17:58:59 -0400

    solution

--- index.js
+++ index.js
@@ -5,3 +5,18 @@ Write a program that prints the numbers from 1 to 100.
 For numbers which are multiples of both three and five print “FizzBuzz”
 */
 
+// test by running node index.js in the terminal
+// What made this challenging?
+// How do you explain this problem to a colleague?
+
+for(let i = 1; i <= 100; i++){
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log('FizzBuzz');
+    } else if ( i % 3 === 0) {
+        console.log('Fizz');
+    } else if ( i % 5 === 0 ) {
+        console.log('Buzz');
+    } else {
+        console.log(i);
+    }
+}
