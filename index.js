d6fcc8f25a81000dedd1b4f812edded98429ed6e
Brett Cunningham <brett.cunningham@sbevision.com>
2020-04-30 13:15:02 -0400

    bad solution doesnt fit prompt

--- index.js
+++ index.js
@@ -5,3 +5,34 @@
 // countHi2("ahibhi") → 2
 // countHi2("xhixhi") → 0
 
+const countHi2 = str => {
+let currentCount = 0;
+  // need inner recursive function 
+  const countFunc = (str, count) => {
+    let newCount = count
+    // check if first 2 chars are 'hi'
+    if(str.slice(0,2) === 'hi') {
+      // if there are other chars recurse on remaining
+      if ( str.slice(2)) {
+        newCount += 1;
+       currentCount += countFunc(str.slice(2), newCount)
+      } else {
+        currentCount += newCount + 1;
+      }
+    } else {
+      if ( str.slice(1)) {
+        countFunc(str.slice(1), newCount)
+      } {
+        return currentCount
+      }
+    }
+
+    return currentCount
+  }
+  countFunc(str, 0)
+  return currentCount
+
+}
+
+countHi2("xhixhi")
+console.log('countHi2("xhixhi")', countHi2("xhixhi"))
