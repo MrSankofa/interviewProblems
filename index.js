fb243661d32bc2c1274b63a6b402d7691e8ee9ca
Brett Cunningham <brett.cunningham@sbevision.com>
2020-05-01 11:00:29 -0400

    prompt

---
+++ index.js
@@ -0,0 +1,14 @@
+
+// Given a string and a non-empty substring sub, compute recursively the number of times that sub appears in the string, without the sub strings overlapping.
+
+
+// strCount("catcowcat", "cat") → 2
+// strCount("catcowcat", "cow") → 1
+// strCount("catcowcat", "dog") → 0
+
+
+
+console.log('strCount("catcowcat", "cat")', strCount("catcowcat", "cat"))
+console.log('strCount("catcowcat", "cow")', strCount("catcowcat", "cow"))
+console.log('strCount("catcowcat", "dog")', strCount("catcowcat", "dog"))
+console.log('strCount("do", "dog")', strCount("catcowcat", "dog"))
